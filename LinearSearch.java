afae7419799f0b932327964465dcfab45ad80403
Rupesh Chidupudi <125015489+chidupudi@users.noreply.github.com>
2024-06-11 00:25:54 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,24 @@
+package Java;
+
+
+class LinearSearch {
+    public static int Linear(int[] a, int find) {
+        for (int i = 0; i < a.length; i++) {
+            int k = a[i];
+            if (k == find) {
+                return i;
+            }
+        }
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        int ar[] = {43, 33, 865, 4356, 2363, 346};
+        int index = Linear(ar, 346);
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
