e47b19f6fa1cd3da17a854c6604fcb54c32912bb
Rupesh Chidupudi <125015489+chidupudi@users.noreply.github.com>
2024-06-12 23:47:45 +0530

    Add files via upload

---
+++ MinMaxArray.java
@@ -0,0 +1,29 @@
+public class MinMaxArray {
+
+    // Method to find the minimum and maximum values in an array
+    public static int[] findMinMax(int[] array) {
+        // Initialize min and max with the first element of the array
+        int min = array[0];
+        int max = array[0];
+
+        // Iterate through the array to find min and max
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        return new int[]{min, max};
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 5, 7, 2, 8, -1, 4, 10, 12};
+        
+        int[] minMax = findMinMax(array);
+        System.out.println("Minimum value: " + minMax[0]);
+        System.out.println("Maximum value: " + minMax[1]);
+    }
+}
