72436c7556846f275f2a6c38db9586547f61b0e3
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-08-11 22:46:39 +0530

    BUGS

--- Factorial.java
+++ Factorial.java
@@ -9,6 +9,6 @@ public class Factorial {
 
     public static void main(String[] args) {
         int number = 5;
-        System.out.println("Factorial of " + number + " is " + factorial(number));
+        System.out.println("Factorial of " + number + " is " + factorial (number));
     }
 }
