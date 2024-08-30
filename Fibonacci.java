66e5770e599c2f8dbed44c98477d85b04daaa3f9
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-08-30 14:35:30 +0530

    changes

--- Fibonacci.java
+++ Fibonacci.java
@@ -8,6 +8,7 @@ public class Fibonacci {
         int a = 0, b = 1;
         System.out.print("Fibonacci series: " + a + " " + b);
 
+        
         for (int i = 2; i < n; i++) {
             int next = a + b;
             System.out.print(" " + next);
