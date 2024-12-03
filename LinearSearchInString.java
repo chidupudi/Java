d4275afecf6f47ed5d1a39bc456478f4c566cc12
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-12-03 23:24:31 +0530

    Initial commit

--- LinearSearchInString.java
+++ LinearSearchInString.java
@@ -24,7 +24,6 @@ public class LinearSearchInString {
                     break;
                 }
             }
-            
             if (j == searchLength) {
                 return i; // Substring found at index i
             }
